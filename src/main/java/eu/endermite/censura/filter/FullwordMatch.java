be0bdcfb90ea865b3f0a7dc1a0da9766deac9a26
TheEpicBlock <git.teb@theepicblock.nl>
2021-03-16 11:56:03 +0100

    Improve Fullword matching algorithm

--- src/main/java/eu/endermite/censura/filter/FullwordMatch.java
+++ src/main/java/eu/endermite/censura/filter/FullwordMatch.java
@@ -4,7 +4,7 @@ public class FullwordMatch implements MatchType {
     private final char[] snippetChars;
 
     public FullwordMatch(String input) {
-        this.snippetChars = input.toCharArray();
+        this.snippetChars = (" "+input+" ").toCharArray();
     }
 
 // The snippet must start and end on a word boundary. Besides that the snippet may be interrupted by any non-alphabetic character. A character may also be repeated.
@@ -19,39 +19,33 @@ public class FullwordMatch implements MatchType {
 
     @Override
     public boolean match(String message, FilterCache cache) {
-        int state = 0;
-        boolean wasSpacer = true; //start of string counts as spacer
+        int state = 1;
 
         for (char c : message.toCharArray()) {
             if (state >= snippetChars.length) {
-                if (isSpacer(c)) {
-                    return true; //We've reached the end and reached a spacer
-                }
-            } else if (match(c, snippetChars[state])) {
-                if (state == 0) {
-                    //Can only match the first letter of the snippet after a space
-                    if (wasSpacer) state++;
-                } else {
-                    state++;
-                }
+                return true;
             }
-            if (state > 0 && !match(c, snippetChars[state - 1]) && !isSpacer(c)) {
-                //This is not a repeated character. We should reset
+            if (compare(c, snippetChars[state])) {
+                state++;
+            } else if (isSpacer(c)) {
+                continue;
+            } else if (state > 0 && compare(c, snippetChars[state-1])) {
+                continue;
+            } else {
                 state = 0;
             }
-
-            wasSpacer = isSpacer(c);
         }
 
-		return state >= snippetChars.length;
-	}
+        return state >= snippetChars.length-1;
+    }
 
     private static boolean isSpacer(char c) {
         return !Character.isAlphabetic(c);
     }
 
-    private static boolean match(char a, char b) {
+    private static boolean compare(char a, char b) {
         if (b == '*') return true;
+        if (b == ' ') return isSpacer(a);
         return a == b;
     }
 }
