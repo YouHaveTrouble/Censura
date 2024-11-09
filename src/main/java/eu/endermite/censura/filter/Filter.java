b3e49793ca881c12ec46b8120bf4191e1620c11a
Firav <firavv@gmail.com>
2024-11-09 11:19:03 -0500

    - Created logic for discord alert function - Modified logic of detect function to trigger discord alert if enabled in config - Import libraries for webhookly - Refactor alert message to include player information, forced player variable for detect function. Can be sent as null object. - Created case handling for null player objects in detect

--- src/main/java/eu/endermite/censura/filter/Filter.java
+++ src/main/java/eu/endermite/censura/filter/Filter.java
@@ -2,10 +2,16 @@ package eu.endermite.censura.filter;
 
 import eu.endermite.censura.Censura;
 import eu.endermite.censura.config.CachedConfig;
+import me.micartey.webhookly.DiscordWebhook;
+import me.micartey.webhookly.embeds.EmbedObject;
+import me.micartey.webhookly.embeds.Footer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.awt.*;
+import java.io.IOException;
 import java.text.Normalizer;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -32,16 +38,25 @@ public class Filter {
         return message;
     }
 
-    public static boolean detect(String message, CachedConfig.FilterCategory filter) {
+    public static boolean detect(String message, CachedConfig.FilterCategory filter, Player player) {
         message = preprocessString(message);
         List<MatchType> matches = filter.getMatches();
+        String alertMessage;
 
         FilterCache cache = new FilterCache();
         for (MatchType match : matches) {
             if (match.match(message, cache)) {
-                if (Censura.getCachedConfig().isLogDetections())
-                    Censura.getPlugin().getLogger().info(String.format("Detected \"%s\" in phrase \"%s\" (type: %s)", match.getSnippet(), message, match.getType()));
-                return true;
+                if (Censura.getCachedConfig().isLogDetections()) {
+                    if (player != null)
+                        alertMessage = String.format("Detected player `%s` stated `%s` in phrase `%s` (type: %s)", player.getName(), match.getSnippet(), message, match.getType());
+                    else
+                        alertMessage = String.format("Detected `%s` in phrase `%s` (type: %s)", match.getSnippet(), message, match.getType());
+                    Censura.getPlugin().getLogger().info(alertMessage);
+                    if (Censura.getCachedConfig().isDiscord())
+                        Censura.getPlugin().getLogger().info("Sending discord trigger");
+                        sendWebhook(alertMessage);
+                }
+                    return true;
             }
         }
         return false;
@@ -55,7 +70,7 @@ public class Filter {
             return false;
 
         for (CachedConfig.FilterCategory filter : Censura.getCachedConfig().getCategories()) {
-            if (detect(message, filter)) {
+            if (detect(message, filter, player)) {
                 doActions(filter.getPunishments(), player);
                 return true;
             }
@@ -66,7 +81,7 @@ public class Filter {
     public static boolean filterNoActions(String message) {
 
         for (CachedConfig.FilterCategory filter : Censura.getCachedConfig().getCategories()) {
-            if (detect(message, filter))
+            if (detect(message, filter, null))
                 return true;
         }
         return false;
@@ -84,6 +99,27 @@ public class Filter {
                 String msg = message.replaceAll("%player%", player.getName());
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
             }
+        }
+    }
+
+    public static void sendWebhook(String message) {
+        DiscordWebhook webhook = new DiscordWebhook(Censura.getCachedConfig().getDiscordURL());
+        webhook.setAvatarUrl(Censura.getCachedConfig().getDiscordAuthorAvatar());
+        webhook.setUsername(Censura.getCachedConfig().getDiscordAuthor());
+
+        Censura.getPlugin().getLogger().info(Censura.getCachedConfig().getDiscordURL() + Censura.getCachedConfig().getDiscordAuthorAvatar() + Censura.getCachedConfig().getDiscordAuthor());
+
+        EmbedObject embed = new EmbedObject()
+                .setTitle("Censure Alert")
+                .setColor(new Color(174, 63 ,65))
+                .setDescription(message)
+                .setFooter(new Footer("Powered by Censura", ""));
+
+        webhook.getEmbeds().add(embed);
+        try {
+            webhook.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
